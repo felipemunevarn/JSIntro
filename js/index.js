c4518fe9678013722529d274b9a5e62a469574f9
felipemunevarn <afmunene@gmail.com>
2022-01-17 00:30:20 -0300

    initial commit

---
+++ js/index.js
@@ -0,0 +1,27 @@
+let title = document.querySelector(".title");
+title.textContent = "Nutrition Good Life";
+
+let pablo = document.querySelector("#pablo");
+let tdWeight = pablo.querySelector(".info-peso");
+let tdHeight = pablo.querySelector(".info-altura");
+let tdImc = pablo.querySelector(".info-imc");
+
+weight = tdWeight.textContent;
+height = tdHeight.textContent;
+
+let imc, isWeightValid = true, isHeightValid = true;
+
+if (weight <= 0 || weight > 1000) {
+    tdImc.textContent = "Incorrect Weight";
+    isWeightValid = false;
+}
+
+if (height <= 0 || height > 3) {
+    tdImc.textContent = "Incorrect Height";
+    isHeightValid = false;
+}
+
+if (isWeightValid && isHeightValid){
+    imc = weight / (height * height);
+    tdImc.textContent = imc;
+}
